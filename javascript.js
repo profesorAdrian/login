18cff74b7c8a867f75c58bb2042b7568b87939ae
profesorAdrian <95009882+profesorAdrian@users.noreply.github.com>
2021-11-25 13:04:40 +0800

    Create javascript file

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var user = 'Adrian'
+var password = '1'
+
+function login(){
+    const formUsername = document.getElementById('username').value
+    const formPassword = document.getElementById('password').value
+
+    if(user == formUsername){
+        if(password == formPassword){
+            printMessage(`Welcome ${formUsername}`)
+        }
+        else{
+            printMessage(`Welcome ${formUsername}`)
+        }
+    }
+    else{
+            printMessage(`Welcome ${formUsername}`)
+    }    
+}
+
+function printMessage(message){
+    const messageDiv = document.getElementById('message')
+
+    messageDiv.innerHTML = message
+}
